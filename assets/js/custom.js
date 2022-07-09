e15555b0892f456925042836777158b9b87fb1e5
RoyJWilliams <108959440+RoyJWilliams@users.noreply.github.com>
2022-07-09 01:10:54 -0400

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,7 +12,7 @@
 			
 			$(".countdowntimer").countdown('2023/02/18 07:21', function(event) {
 				$(this).html(event.strftime(
-					'<div class="count"><h2><strong>Emma&apos;s Birthday</strong></h2>Saturday 18th February 2023<br><br><h1><b>%-D Days</h1></b><h4>%-Hh : %-Mm : %Ss</h4>' +
+					'<div class="count"><h2><strong>Countdown Template</strong></h2>Saturday 18th February 2023<br><br><h1><b>%-D Days</h1></b><h4>%-Hh : %-Mm : %Ss</h4>' +
 					''
 				));
 			});
