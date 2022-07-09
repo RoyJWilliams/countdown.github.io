6a583f4e2cd173a557b249f7a14dc750750e7945
RoyJWilliams <108959440+RoyJWilliams@users.noreply.github.com>
2022-07-09 01:08:28 -0400

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,9 +10,9 @@
 
 		if (typeof $.fn.countdown !== 'undefined') {
 			
-			$(".countdowntimer").countdown('2023/02/17 07:21', function(event) {
+			$(".countdowntimer").countdown('2023/02/18 07:21', function(event) {
 				$(this).html(event.strftime(
-					'<div class="count"><h2><strong>COUNTDOWN TEMPLATE V1</strong></h2>Thursday 18th February 2023<br><br><h1><b>%-D Days</h1></b><h4>%-Hh : %-Mm : %Ss</h4>' +
+					'<div class="count"><h2><strong>Emma&apos;s Birthday</strong></h2>Saturday 18th February 2023<br><br><h1><b>%-D Days</h1></b><h4>%-Hh : %-Mm : %Ss</h4>' +
 					''
 				));
 			});
